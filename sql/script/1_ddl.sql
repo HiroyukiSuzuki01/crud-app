d54ee174aafd16f56d17cf142a389704310996ab
HiroyukiSuzuki01 <hiroyuki.suzuki9999@gmail.com>
2022-10-20 11:41:28 +0900

    hobbiesテーブル追加

--- sql/script/1_ddl.sql
+++ sql/script/1_ddl.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE IF NOT EXISTS crud_app;
 
 CREATE TABLE IF NOT EXISTS crud_app.prefectures(id int auto_increment primary key, name varchar(10) not null, created_at datetime, updated_at datetime);
+
+CREATE TABLE IF NOT EXISTS crud_app.hobbies(id int auto_increment primary key, name varchar(10) not null, created_at datetime, updated_at datetime);
